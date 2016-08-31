1c71b38fb03e487535bd11f130f536164dac693c
Joshua Efiong <joshefiong@gmail.com>
2016-08-31 17:47:30 +0100

    Wrote a method to create decorators that can be used in place of mixins.

---
+++ src/utils/mixin.js
@@ -0,0 +1,53 @@
+import Symbol from "es6-symbol";
+import ownKeys from "reflect.ownkeys";
+
+export function mixin(behaviour, sharedBehaviour = {}) {
+  // these keys reflect the behaviour that is to be attached to class instances
+  const instanceKeys = ownKeys(behaviour);
+  // these keys reflect static behaviour
+  const sharedKeys = ownKeys(sharedBehaviour);
+  const typeTag = Symbol("isA");
+
+  function _mixin(workingClass) {
+    // attach instance-oriented behaviour
+    for (let property of instanceKeys) {
+      Object.defineProperty(
+        workingClass.prototype,
+        property,
+        {
+          value: behaviour[property],
+          writable: true,
+        }
+      );
+    }
+
+    Object.defineProperty(workingClass.prototype, typeTag, { value: true });
+  }
+
+  // attach static behaviour
+  for (let property of sharedKeys) {
+    Object.defineProperty(
+      _mixin,
+      property,
+      {
+        enumerable: sharedBehaviour.propertyIsEnumerable(property),
+        value: sharedBehaviour[property],
+        writable: true,
+      }
+    );
+  }
+
+  // this allows you to use "instanceof" on an object that uses a mixin
+  Object.defineProperty(
+    _mixin,
+    Symbol.hasInstance,
+    {
+      value: (instance) => !!instance[typeTag],
+    writable: true,
+}
+);
+
+  return _mixin;
+}
+
+export default mixin;
