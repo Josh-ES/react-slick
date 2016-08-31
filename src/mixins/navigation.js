1b741eb34384cfa1f8484cf176f124eb3dc027bc
Joshua Efiong <joshefiong@gmail.com>
2016-08-31 20:57:15 +0100

    Implemented getNavigableIndexes method.

--- src/mixins/navigation.js
+++ src/mixins/navigation.js
@@ -3,7 +3,30 @@
 import mixin from '../utils/mixin';
 
 const navigation = mixin({
+  getNavigableIndexes() {
+    let max;
+    let breakPoint = 0;
+    let counter = 0;
+    let indexes = [];
 
+    if (!this.props.infinite) {
+      max = this.state.slideCount;
+    } else {
+      breakPoint = this.props.slidesToShow * -1;
+      counter = this.props.slidesToShow * -1;
+      max = this.state.slideCount * 2;
+    }
+
+    while (breakPoint < max) {
+      indexes.push(breakPoint);
+      breakPoint = counter + this.props.slidesToScroll;
+
+      counter += this.props.slidesToScroll <= this.props.slidesToShow ?
+        this.props.slidesToScroll : this.props.slidesToShow;
+    }
+
+    return indexes;
+  },
 });
 
 export default navigation;
